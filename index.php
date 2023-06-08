ac99090925e51adf20885c52c3b0df2a92b797fe
apademide <contact@apademide.ch>
2023-06-08 13:43:36 +0200

    style: clean unused code

--- index.php
+++ index.php
@@ -8,36 +8,6 @@ load([
 ], __DIR__);
 
 Kirby::plugin("auaust/tags", [
-  'fields' => [
-    // A selector field for tags
-    'tags' => [
-      'props' => [
-        'tagset' => function (string $set = null) {
-          // // Tags are stored in a content object.
-          // // This by default is the site object, but it's recommended to use a parent page, i.e. a page named "tags".
-          // if (($parent = option('auaust.tags.storage.parent')) === null) {
-          //   throw new Exception('No parent set for tags field.');
-          // };
-
-          // $default = option('auaust.tags.default');
-          // $key = option('auaust.tags.prefix') . ($set ?? $default);
-
-          // $isDefault = $set === null || $set === $default;
-
-
-          // if ($parent->content()->get($key) === null) {
-          // }
-
-          return $set ?? option('auaust.tags.default');
-        },
-      ],
-      'computed' => [
-        'value' => function () {
-          return $this->tagset;
-        },
-      ],
-    ]
-  ],
   'pageModels' => [
     'tag' => 'TagPage',
     'tagset' => 'TagSetPage',
