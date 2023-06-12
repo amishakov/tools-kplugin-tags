4d02b20e865faf5c9bc04d3ea0ca80ec0afb7395
apademide <contact@apademide.ch>
2023-06-12 16:04:46 +0200

    fix: wrap blueprint in a callback to use `option()`

--- blueprints/pages/tagset.php
+++ blueprints/pages/tagset.php
@@ -1,25 +1,27 @@
 <?php
 
-return [
-  'title' => [
-    'fr' => 'Groupe de tags',
-    'en' => 'Tag set'
-  ],
-  'icon' => 'tag',
-  'columns' => [
-    [
-      'width' => '1/2',
-      'sections' => [
-        'tags' => [
-          'type' => 'fields',
-          'fields' => [
-            'tags' => [
-              'type' => 'pages',
-              'query' => 'site.find("tags").children'
+return function ($kirby) {
+  return [
+    'title' => [
+      'fr' => 'Groupe de tags',
+      'en' => 'Tag set'
+    ],
+    'icon' => 'tag',
+    'columns' => [
+      [
+        'width' => '1/2',
+        'sections' => [
+          'tags' => [
+            'type' => 'fields',
+            'fields' => [
+              'tags' => [
+                'type' => 'pages',
+                'query' => 'site.find("' . kirby()->option('auaust.tags.tagsPage')->slug() . '").children'
+              ]
             ]
           ]
         ]
       ]
     ]
-  ]
-];
+  ];
+};
