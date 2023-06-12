baa02d5103e31aa74850498a6bcfd60ae50b7f78
apademide <contact@apademide.ch>
2023-06-12 17:49:13 +0200

    imp: reenable tagsets() method

--- models/TagPage.php
+++ models/TagPage.php
@@ -4,14 +4,14 @@ use Kirby\Cms\Page;
 
 class TagPage extends Page
 {
-  // public function tagsets()
-  // {
-  //   $tag = $this;
-  //   $tagsets = option('auaust.tags.tagSetsPage')->children()->filter(
-  //     function ($tagset) use ($tag) {
-  //       return $tagset->tags()->toPages()->has($tag);
-  //     }
-  //   );
-  //   return $tagsets;
-  // }
+  public function tagsets()
+  {
+    $tag = $this;
+    $tagsets = option('auaust.tags.tagSetsPage')->children()->filter(
+      function ($tagset) use ($tag) {
+        return $tagset->tags()->toPages()->has($tag);
+      }
+    );
+    return $tagsets;
+  }
 }
