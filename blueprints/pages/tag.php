05a709cf2b15f652ee85f49554e8bce4766f51ff
apademide <contact@apademide.ch>
2023-06-13 13:44:20 +0200

    style: wrap tag blueprint in a callback

--- blueprints/pages/tag.php
+++ blueprints/pages/tag.php
@@ -1,49 +1,51 @@
 <?php
 
-return [
-  'title' => [
-    'fr' => 'Tag',
-    'en' => 'Tag'
-  ],
-  'icon' => 'tag',
-  'columns' => [
-    [
-      'width' => '2/3',
-      'sections' => [
-        'infos' => [
-          'type' => 'fields',
-          'fields' => [
-            'description' => [
-              'type' => 'textarea',
-              'label' => [
-                'fr' => 'Description',
-                'en' => 'Description'
+return function () {
+  return [
+    'title' => [
+      'fr' => 'Tag',
+      'en' => 'Tag'
+    ],
+    'icon' => 'tag',
+    'columns' => [
+      [
+        'width' => '2/3',
+        'sections' => [
+          'infos' => [
+            'type' => 'fields',
+            'fields' => [
+              'description' => [
+                'type' => 'textarea',
+                'label' => [
+                  'fr' => 'Description',
+                  'en' => 'Description'
+                ],
+                'buttons' => [
+                  'bold',
+                  'italic',
+                  '|',
+                  'link',
+                  '|',
+                ],
+                'size' => 'medium'
               ],
-              'buttons' => [
-                'bold',
-                'italic',
-                '|',
-                'link',
-                '|',
-              ],
-              'size' => 'medium'
-            ],
-            'tagsets' => [
-              'type' => 'pages',
-              'query' => 'site.find("tagsets").children',
-              'translate' => false,
-              'help' => [
-                'fr' => '' .
-                  'Tous les groupes de tags auxquel ce tag peut être associé.' . PHP_EOL .
-                  '**NB**: Les catégories de tags ne sont pas traduites. Assurez-vous d\'être dans la traduction anglaise pour modifier cette sélection.',
-                'en' => '' .
-                  'All tag sets that this tag can be associated with.' . PHP_EOL .
-                  '**NB**: Tag categories are not translated. Make sure you are in the English translation to edit this selection.'
+              'tagsets' => [
+                'type' => 'tagsTagsets',
+                'query' => 'site.find("tagsets").children',
+                'translate' => false,
+                'help' => [
+                  'fr' => '' .
+                    'Tous les groupes de tags auxquel ce tag peut être associé.' . PHP_EOL .
+                    '**NB**: Les catégories de tags ne sont pas traduites. Assurez-vous d\'être dans la traduction anglaise pour modifier cette sélection.',
+                  'en' => '' .
+                    'All tag sets that this tag can be associated with.' . PHP_EOL .
+                    '**NB**: Tag categories are not translated. Make sure you are in the English translation to edit this selection.'
+                ]
               ]
             ]
           ]
         ]
       ]
     ]
-  ]
-];
+  ];
+};
