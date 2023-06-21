ee5fe9df19f38fbb4f0bca7a4ac4c11bca667e2a
apademide <contact@apademide.ch>
2023-06-21 09:29:15 +0200

    imp: disable not really helpful help section

--- blueprints/pages/tags.php
+++ blueprints/pages/tags.php
@@ -12,28 +12,28 @@ return [
     'changeStatus' => false,
   ],
   'columns' => [
-    [
-      'width' => '1/1',
-      'sections' => [
-        'info' => [
-          'type' => 'info',
-          'label' => [
-            'fr' => 'Logique des tags',
-            'en' => 'Tags logic'
-          ],
-          'text' => [
-            'fr' => '' .
-              'L\'idée des tags est de pouvoir connecter des produits entre eux par les thèmes qu\'ils partagent. Pour les optimiser, il faudrait que chaque tag soit assez spécifique.' . PHP_EOL .
-              'Par exemple, un livre sur les chats pourrait avoir le tag "chat", mais on évitera d\'avoir directement le tag "animaux". ' .
-              'Pour compenser la spécificité des tags, les catégories plus générales sont regroupées dans des groupes de tags. Ainsi, le tag "animaux" serait omis de l\'exemple précédent, car le tag "chat" est lui-même dans le groupe de tags "animaux" et donc hérite implicitement de cette classification.',
-            'en' =>  '' .
-              'The idea of tags is to connect products by the themes they share. To optimize them, each tag should be specific enough.' . PHP_EOL .
-              'For example, a book about cats could have the tag "cat", but we will avoid having the tag "animals" directly.' .
-              'To compensate for the specificity of tags, more general categories are grouped into tag sets. Thus, the tag "animals" would be omitted from the previous example, because the tag "cat" is itself in the tag set "animals" and therefore implicitly inherits this classification.'
-          ]
-        ]
-      ]
-    ],
+    // [
+    //   'width' => '1/1',
+    //   'sections' => [
+    //     'info' => [
+    //       'type' => 'info',
+    //       'label' => [
+    //         'fr' => 'Logique des tags',
+    //         'en' => 'Tags logic'
+    //       ],
+    //       'text' => [
+    //         'fr' => '' .
+    //           'L\'idée des tags est de pouvoir connecter des produits entre eux par les thèmes qu\'ils partagent. Pour les optimiser, il faudrait que chaque tag soit assez spécifique.' . PHP_EOL .
+    //           'Par exemple, un livre sur les chats pourrait avoir le tag "chat", mais on évitera d\'avoir directement le tag "animaux". ' .
+    //           'Pour compenser la spécificité des tags, les catégories plus générales sont regroupées dans des groupes de tags. Ainsi, le tag "animaux" serait omis de l\'exemple précédent, car le tag "chat" est lui-même dans le groupe de tags "animaux" et donc hérite implicitement de cette classification.',
+    //         'en' =>  '' .
+    //           'The idea of tags is to connect products by the themes they share. To optimize them, each tag should be specific enough.' . PHP_EOL .
+    //           'For example, a book about cats could have the tag "cat", but we will avoid having the tag "animals" directly.' .
+    //           'To compensate for the specificity of tags, more general categories are grouped into tag sets. Thus, the tag "animals" would be omitted from the previous example, because the tag "cat" is itself in the tag set "animals" and therefore implicitly inherits this classification.'
+    //       ]
+    //     ]
+    //   ]
+    // ],
     [
       'width' => '1/2',
       'sections' => [
